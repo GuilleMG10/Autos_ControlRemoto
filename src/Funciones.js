3eed5d7eeb9945da307a2321e007c750772248d1
Guillermo <guichi.maldo@gmail.com>
2023-09-03 15:17:31 -0400

    Funcion que verifica si el formato de entrada es correcto

--- src/Funciones.js
+++ src/Funciones.js
@@ -3,4 +3,16 @@ function posicionInicial(posicionX,posicionY,orientacion)
     return [posicionX,posicionY,orientacion];
 }
 
-export {posicionInicial};
+function VerificarFormato_Entrada(posicionX,posicionY,orientacion)
+{
+    if(Number.isInteger(posicionX) && Number.isInteger(posicionY) && typeof orientacion === 'string' )
+    {
+       if(orientacion=="N" || orientacion=="S" ||orientacion=="O"||orientacion=="E")
+       {
+        return true;
+       }  
+    }
+    return false;
+}
+
+export {posicionInicial,VerificarFormato_Entrada};
--- src/Funciones.test.js
+++ src/Funciones.test.js
@@ -1,7 +1,10 @@
-import { posicionInicial } from "./Funciones";
+import { posicionInicial , VerificarFormato_Entrada} from "./Funciones";
 
 describe("Funciones", () => {
     it("deberia Devolver a posicion Inicial", () => {
       expect(posicionInicial(5,1,'N')).toEqual([5,1,'N']);
     });
+    it("deberia decir si el formato es correcto", () => {
+        expect(VerificarFormato_Entrada(5,1,"N")).toEqual(true);
+      });
   });
